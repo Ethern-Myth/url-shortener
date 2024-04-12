38ad243ee23f4ae79ae359592c912f9293ea7ad6
Ethern-Myth <ethernmyth@gmail.com>
2024-04-12 09:54:43 +0200

    Updated the source code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,26 +5,26 @@
  * @param url - The URL to validate.
  * @returns A boolean indicating if the URL is valid.
  */
-export function isValidUrl(url: string): boolean {
-	const pattern = new RegExp(
-		"^(https?:\\/\\/)?" + // protocol
-			"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" + // domain name and extension
-			"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-			"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-			"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-			"(\\#[-a-z\\d_]*)?$",
-		"i"
-	); // fragment locator
-	return !!pattern.test(url);
-}
+// export function isValidUrl(url: string): boolean {
+// 	const pattern = new RegExp(
+// 		"^(https?:\\/\\/)?" + // protocol
+// 			"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" + // domain name and extension
+// 			"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+// 			"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+// 			"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+// 			"(\\#[-a-z\\d_]*)?$",
+// 		"i"
+// 	); // fragment locator
+// 	return !!pattern.test(url);
+// }
 
 // To Avoid REGEX patterns for purpose of not blocking Event loop 
 
-// export function isValidUrl(url: string): boolean {
-//     try {
-//         new URL(url);
-//         return true;
-//     } catch (error) {
-//         return false;
-//     }
-// }
+export function isValidUrl(url: string): boolean {
+    try {
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
